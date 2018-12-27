7d6b4f4a26929d8d4a48bd2daa3f3c406a94e9e1
txk <603085489@qq.com>
2018-12-27 22:41:04 +0800

    CLONE,第二次修改

--- git_demo/src/main/java/com/jishou/Person.java
+++ git_demo/src/main/java/com/jishou/Person.java
@@ -5,4 +5,28 @@ public class Person {
     private String name;
     private String addressp;
     private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressp() {
+        return addressp;
+    }
+
+    public void setAddressp(String addressp) {
+        this.addressp = addressp;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
